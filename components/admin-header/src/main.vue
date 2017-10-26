31335221c4a938eb89099b60368bc40a26689421
华狄 <861883474@qq.com>
2017-10-26 23:16:10 +0800

    fixbug

--- components/admin-header/src/main.vue
+++ components/admin-header/src/main.vue
@@ -1,22 +1,25 @@
 <template>
   <el-col :span="24" class="panel-top">
-  <el-col :span="20" class="f24">
-    <div class="navbar-header pull-left pl20">
-      <img :src="logo" alt="" class="logo round">
-      <span>{{title}}<i style="color:#20a0ff">{{typeName}}</i></span>
-    </div>
-    <div class="pull-left pl20 pr20 pointer f20 navbar-header-btn" @click="fullTodo">
-      <i class="el-icon-menu"></i>
-    </div>
-  </el-col>
-  <el-col :span="4">
-    <div class="clearfix pull-right pointer" @click="logout">
-      {{user}}
-      <el-tooltip class="item tip-logout pl10" effect="dark" content="退出" placement="bottom">
-        <i class="fa fa-sign-out" aria-hidden="true"></i>
-      </el-tooltip>
-    </div>
-  </el-col>
+    <el-col :span="20" class="f24">
+      <div class="navbar-header pull-left pl20">
+        <img :src="logo" alt="" class="logo round">
+        <span>
+          <span v-text="title"></span>
+          <i style="color:#20a0ff">{{typeName}}</i>
+        </span>
+      </div>
+      <div class="pull-left pl20 pr20 pointer f20 navbar-header-btn" @click="fullTodo">
+        <i class="el-icon-menu"></i>
+      </div>
+    </el-col>
+    <el-col :span="4">
+      <div class="clearfix pull-right pointer" @click="logout">
+        {{user}}
+        <el-tooltip class="item tip-logout pl10" effect="dark" content="退出" placement="bottom">
+          <i class="fa fa-sign-out" aria-hidden="true"></i>
+        </el-tooltip>
+      </div>
+    </el-col>
   </el-col>
 </template>
 
